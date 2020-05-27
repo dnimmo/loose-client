a67a391c1f50046f65d52303f089200f766aedd2
David Nimmo <david.nimmo@clicktravel.com>
2020-05-27 16:35:31 +0100

    added SearchBar

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -4,15 +4,35 @@ import styled from "styled-components";
 
 const SearchBarWrapper =
   styled.div`
+    display: grid;
+    grid-template-columns: 95% 5%;
     background-color: #350d36;
     height: 5vh;
     min-height: 40px;
     width: 100%`;
 
 
+const HelpButton = 
+  styled.button`
+  border: 0;
+  background: none;
+  margin: 3px;
+  cursor: pointer;
+  outline: none;`;
+
+const HelpIcon =
+  styled.img`
+  height: 30px;
+  width: 20px;`;
+
+
 const SearchBar = 
   () => 
       <SearchBarWrapper>
+        <span></span>
+        <HelpButton>
+          <HelpIcon src="/images/question.svg"/>
+        </HelpButton>
       </SearchBarWrapper>;
 
 export default SearchBar;
